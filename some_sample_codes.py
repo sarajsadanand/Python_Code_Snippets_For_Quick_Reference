2f3194015efcbc84c00b57c60989e295fd437093
sarajsadanand <sarajsadanand@hotmail.com>
2022-09-10 23:30:14 +0530

    new codes added

--- some_sample_codes.py
+++ some_sample_codes.py
@@ -1,6 +1,5 @@
 ###################################################################################
-
-# importing the module
+'''# importing the module
 import pandas as pd
 
 # creating a sample dataframe
@@ -20,5 +19,53 @@ data = pd.DataFrame({'Brand': ['Maruti', 'Hyundai', 'Tata',
 
 # displaying the DataFrame
 display(data)
+'''
 
 ###################################################################################
+'''
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print('Run...')
+
+
+player1 = PlayerCharacter("Saraj", 22)
+print(player1.name)
+'''
+###################################################################################
+'''def super_func(*args, **kwargs):
+    print(args)   # args as a tuple
+    print(kwargs)  # this returns keyword arguments as a dict
+    total = 0
+    for i in kwargs.values():
+        total += i
+    return sum(args) + total
+
+
+print(super_func(1, 2, 3, 4, 5, num1=10, num2=20))
+'''
+
+
+###################################################################################
+'''
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+def oldest_student(*args):
+    return max(args)
+
+
+s1 = Student('Saraj', 22)
+s2 = Student('San', 33)
+s3 = Student('Sai', 34)
+
+print(f"The oldest student is {oldest_student(s1.age, s2.age, s3.age)} year old.")
+'''
+###################################################################################
+
